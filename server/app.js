32589208429665ed93cceb2fff7f36c23b321d06
Gene Agnew <gragnew@groveoffungus.com>
2024-03-14 18:16:35 -0700

    added routers

--- server/app.js
+++ server/app.js
@@ -1,9 +1,26 @@
 // 'Import' the Express module instead of http
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import pizzas from "./routers/pizzas.js";
 
 // Load environment variables from .env file
 dotenv.config();
+
+// Connect to MongoDB Atlas server
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 // get the PORT from the environment variables, OR use 4040 as default
 const PORT = process.env.PORT || 4040;
@@ -84,6 +101,8 @@ app.get("/weather/:city", (request, response) => {
     city
   });
 });
+
+app.use("/pizzas", pizzas);
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
